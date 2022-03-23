9d62819a95558e138178063eef846d431aa3760f
Dom Bryan <dombguitar@gmail.com>
2022-03-23 09:38:53 +0000

    Initial Commit

---
+++ RollDice/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  RollDice
+//
+//  Created by Dom Bryan on 23/03/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ RollDice/RollDiceApp.swift
@@ -0,0 +1,17 @@
+//
+//  RollDiceApp.swift
+//  RollDice
+//
+//  Created by Dom Bryan on 23/03/2022.
+//
+
+import SwiftUI
+
+@main
+struct RollDiceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
